993c9ae2fdd3735b9123d7f1bd19addef1629482
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2024-11-08 22:40:11 -0600

    get and post requests

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 
 app = FastAPI()
 
@@ -6,6 +7,16 @@ app = FastAPI()
 @app.get("/") #the decorator
 async def root(): #the function
     return {"message": "hello"}
+
+@app.get("/posts")
+def get_posts():
+    return {"Pretend there are posts here"}
+
+@app.post("/createpost")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"new_post": f"title {payload['title']} content {payload['content']}"}
+
 
 #async is optional, only needed for asyncronous tasks, tasks that take a certain amount of time: api calls, talking to database
 #root name is arbatrary; keep names descriptive
