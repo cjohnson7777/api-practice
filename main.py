5086fb0b47bc5cf4b28ba4f77b4bed087283c392
Christina Johnson <95447721+cjohnson7777@users.noreply.github.com>
2024-11-18 00:40:05 -0600

    worked on schemas

--- main.py
+++ main.py
@@ -1,7 +1,16 @@
+from typing import Optional
 from fastapi import FastAPI
 from fastapi.params import Body
+from pydantic import BaseModel #helps develop schemas
 
 app = FastAPI()
+
+class Post(BaseModel):
+    title: str
+    content: str
+    published: bool = True
+    rating: Optional[int] = None
+
 
 #path operation or route; 2 parts the function and the decorator
 @app.get("/") #the decorator
@@ -13,9 +22,9 @@ def get_posts():
     return {"Pretend there are posts here"}
 
 @app.post("/createpost")
-def create_posts(payload: dict = Body(...)):
-    print(payload)
-    return {"new_post": f"title {payload['title']} content {payload['content']}"}
+def create_posts(new_post: Post):
+    print(new_post.dict())    
+    return {"data": new_post}
 
 
 #async is optional, only needed for asyncronous tasks, tasks that take a certain amount of time: api calls, talking to database
